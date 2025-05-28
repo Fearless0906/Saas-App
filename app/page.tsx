dbc3dcfb906079cd32fc68b7bcd708c56365c83d
Fearless0906 <jasonpiit9@gmail.com>
2025-05-28 17:28:02 +0800

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">My Saas App</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default Page;
